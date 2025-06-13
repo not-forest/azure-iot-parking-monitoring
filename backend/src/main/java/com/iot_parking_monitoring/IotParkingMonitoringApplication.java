4cd27182dda80793696817cf0a87336af08dcddf
nuochka <anstskovalenko@gmail.com>
2025-06-13 17:11:00 +0200

    Added docker file and deployment configuration for azure.

--- backend/src/main/java/com/iot_parking_monitoring/IotParkingMonitoringApplication.java
+++ backend/src/main/java/com/iot_parking_monitoring/IotParkingMonitoringApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import com.iot_parking_monitoring.receivers.Receiver;
-import io.github.cdimascio.dotenv.Dotenv;
 import com.iot_parking_monitoring.controllers.WebSocketController;
 
 @SpringBootApplication
@@ -12,9 +11,8 @@ public class IotParkingMonitoringApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(IotParkingMonitoringApplication.class, args);
 
-        Dotenv dotenv = Dotenv.load();
-        String connectionString = dotenv.get("EVENT_HUB_CONNECTION_STRING");
-        String eventHubName = dotenv.get("EVENT_HUB_NAME");
+        String connectionString = System.getenv("EVENT_HUB_CONNECTION_STRING");
+        String eventHubName = System.getenv("EVENT_HUB_NAME");
         WebSocketController webSocketController = context.getBean(WebSocketController.class);
         Receiver receiver = new Receiver(connectionString, eventHubName, webSocketController);
         receiver.start();
